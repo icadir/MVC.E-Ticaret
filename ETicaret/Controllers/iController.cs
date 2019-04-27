98676e2d15d17707372997ac50b7cd8576f2229a
İsmail Çadır <44619941+icadir@users.noreply.github.com>
2019-04-27 17:34:11 +0300

    İlkDeneme

---
+++ ETicaret/Controllers/iController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ETicaret.Controllers
+{
+    public class iController : Controller
+    {
+        // GET: i
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

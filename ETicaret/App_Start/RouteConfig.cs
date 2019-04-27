1eaaadaf846f00bede6808e4b0ce354f3b610708
İsmail Çadır <44619941+icadir@users.noreply.github.com>
2019-04-27 18:07:03 +0300

    Layout Sayfası Tasarım Ok

--- ETicaret/App_Start/RouteConfig.cs
+++ ETicaret/App_Start/RouteConfig.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿
 using System.Web.Mvc;
 using System.Web.Routing;
 
@@ -16,7 +13,7 @@ namespace ETicaret
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "i", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
